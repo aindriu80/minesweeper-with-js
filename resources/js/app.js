4dba251b8b007d1818010675e8ee916664926b22
aindriu80 <aindriu80@gmail.com>
2020-08-21 21:23:56 +0100

    Minesweeper layout & creating Array

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let width = 10;
+  let bombAmount = 20;
+  let squares = [];
+
+  // creating the Board
+  function createBoard() {
+    // get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill("bomb");
+    const emptyArray = Array(width * width - bombAmount).fill("valid");
+    const gameArray = emptyArray.concat(bombsArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+    console.log(shuffledArray);
+
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("id", i);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+});
